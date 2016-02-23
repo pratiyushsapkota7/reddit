aaf0917b8cd22e6bfe5f98914adc933d565458bf
pratiyush sapkota <pratiyushsapkota7@gmail.com>
2016-02-23 12:30:24 -0500

    Add devise and create model

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   resources :links
   
-  root to: "links#index"
+  root "links#index"
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
